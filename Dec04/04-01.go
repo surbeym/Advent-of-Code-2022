8c4e1761a325a8f9b9e2d9e872c39fd8f0e2655e
Surbey <msurbey@gmail.com>
2022-12-27 12:47:19 -0700

    Dec04 01

--- Dec04/04-01.go
+++ Dec04/04-01.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	readFile, err := os.Open("Dec03/input.txt")
+	readFile, err := os.Open("Dec04/input.txt")
 
 	if err == nil {
 		fmt.Println(err)
@@ -16,7 +18,71 @@ func main() {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	for fileScanner.Scan() {
+	count := 0
 
+	for fileScanner.Scan() {
+		pair := fileScanner.Text()
+
+		p := strings.Split(pair, ",")
+
+		c1 := sectionNumbers(p[0])
+		c2 := sectionNumbers(p[1])
+
+		all := make([]int, 0)
+
+		all = append(all, c1...)
+		all = append(all, c2...)
+
+		overlap := dupicates(all)
+
+		if len(c1) > len(c2) {
+			if overlap == len(c2) {
+				count++
+			}
+		} else {
+			if overlap == len(c1) {
+				count++
+			}
+		}
 	}
+
+	println(count)
+}
+
+func dupicates(l []int) int {
+	res := make(map[int]int)
+	for i := 0; i < len(l); i++ {
+		n := l[i]
+		_, present := res[n]
+
+		if present {
+			res[n] = res[n] + 1
+		} else {
+			res[n] = 1
+		}
+	}
+
+	count := 0
+	for _, v := range res {
+		if v > 1 {
+			count++
+		}
+	}
+
+	return count
+}
+
+func sectionNumbers(s string) []int {
+	sl := strings.Split(s, "-")
+	start, _ := strconv.Atoi(sl[0])
+	end, _ := strconv.Atoi(sl[1])
+
+	c := end - start
+
+	res := make([]int, end-start+1)
+	for i := 0; i <= c; i++ {
+		res[i] = i + start
+	}
+
+	return res
 }
